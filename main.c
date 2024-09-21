c1b789412b517e434382411e8c7dffbbe1857f19
veraxqy <elderbruno2014@gmail.com>
2024-09-21 14:39:02 -0300

    Criação do Menu Sobre no main.c

--- main.c
+++ main.c
@@ -9,30 +9,53 @@
 
 // BIBLIOTECAS
 #include <stdio.h>
-#include <windows.h>
+//#include <windows.h>
 
 
 // FUNÇÕES
 void menu_principal(void);
+void menu_sobre(void);
 
 // FUNÇÃO MAIN
 int main(void) {
-    SetConsoleOutputCP(CP_UTF8);
+    //SetConsoleOutputCP(CP_UTF8);
     menu_principal();
+    menu_sobre();
 }
 
 // MENU_PRINCIPAL
 void menu_principal(void) {
+    printf("_____------------------------------------------------_____\n");
+    printf("|   |              == SIG-Rent-a-Car ==              |   |\n");
+    printf("|   |         Sistema de Locação de Veículos         |   |\n");
+    printf("---------------------------------------------------------\n");
+    printf("|                                                        |\n");
+    printf("|                    1 - Pessoas                         |\n");
+    printf("|                    2 - Veículos                        |\n");
+    printf("|                    3 - Serviços                        |\n");
+    printf("|                    4 - Sobre                           |\n");
+    printf("|                    0 - Sair                            |\n");
+    printf("|                                                        |\n");
+    printf("----------------------------------------------------------\n");
+    printf("\n");
+    printf("-> Escolha sua opção: \n");
+}
+
+// MENU_SOBRE
+void menu_sobre(void) {
     printf("_____--------------------------------------------------------_____\n");
     printf("|   |                  == SIG-Rent-a-Car ==                  |   |\n");
     printf("|   |             Sistema de Locação de Veículos             |   |\n");
     printf("------------------------------------------------------------------\n");
-    printf("|                 1 - Pessoas                                    |\n");
-    printf("|                 2 - Veículos                                   |\n");
-    printf("|                 3 - Serviços                                   |\n");
-    printf("|                 4 - Sobre                                      |\n");
-    printf("|                 0 - Sair                                       |\n");
+    printf("|   |                         SOBRE                          |   |\n");
     printf("------------------------------------------------------------------\n");
-    printf("\n");
-    printf("-> Escolha sua opção: \n");
+    printf("|                                                                |\n");
+    printf("| Esse é um projeto referente à disciplina DCT1106 - PROGRAMAÇÃO |\n");
+    printf("|   do curso de Sistemas de Informação, da Universidade Federal  |\n");
+    printf("|      do Rio Grande do Norte (UFRN). Seu objetivo é criarmos    |\n");
+    printf("|    um Sistema para gestão de Locação de Veículos, utilizando   |\n");
+    printf("|    o conhecimento adquirido durante o todo o 2° semestre do    |\n");
+    printf("|          curso, como forma de avaliação. \u00A9 2024           |\n");
+    printf("|                                                                |\n");
+    printf("------------------------------------------------------------------\n");
 }
