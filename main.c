fea938bcce83d0bfd8aa2f40a00fe07a3ee8bae9
veraxqy <elderbruno2014@gmail.com>
2024-10-20 17:02:53 -0300

    Alteração das assinaturas do main.c para funcoes.h

--- main.c
+++ main.c
@@ -11,31 +11,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// ATIVANDO FUNÇÕES
-void menu_principal(void);
-void menu_sobre(void);
-
-    // PESSOAS
-void menu_pessoas(void);
-void menu_cadastrar_pessoa(void);
-void menu_checar_pessoa(void);
-void menu_alterar_pessoa(void);
-void menu_excluir_pessoa(void);
-
-    // VEÍCULOS
-void menu_veiculos(void);
-void menu_cadastrar_veiculo(void);
-void menu_checar_veiculo(void);
-void menu_alterar_veiculo(void);
-void menu_excluir_veiculo(void);
-
-    // LOCAÇÕES
-void menu_locacoes(void);
-void menu_cadastrar_locacao(void);
-void menu_checar_locacao(void);
-void menu_alterar_locacao(void);
-void menu_excluir_locacao(void);
-
 // FUNÇÃO MAIN - PRINCIPAL
 int main(void) {
     menu_principal();
